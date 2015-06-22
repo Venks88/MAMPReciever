d7021e4f5696251a36c3c5129e9088a777328565
Venkata <venkata.narasimhan@adsdata.ca>
2015-06-22 10:03:18 -0400

    Adding Mamp receiver to the git hub

---
+++ app/src/main/java/com/example/venkata/mampreciever/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.venkata.mampreciever;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends ListActivity {
+
+    private ProgressDialog pDialog;
+
+    // URL to get contacts JSON
+    private static String url = "http://10.10.3.182:8888/";
+
+    // JSON Node names
+    private static final String TAG_PERSON = "Animal Type: Dog";
+    private static final String TAG_NAME = "Name";
+    private static final String TAG_EMAIL = "Age";
+
+    // contacts JSONArray
+    JSONArray contacts = null;
+
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> contactList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<HashMap<String, String>>();
+
+        ListView lv = getListView();
+
+        // Calling async task to get json
+        new GetContacts().execute();
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     * */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    contacts = jsonObj.getJSONArray(TAG_PERSON);
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String name = c.getString(TAG_NAME);
+                        String email = c.getString(TAG_EMAIL);
+
+                        // tmp hashmap for single contact
+                        HashMap<String, String> contact = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_EMAIL, email);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[] { TAG_NAME, TAG_EMAIL,
+                    TAG_PERSON }, new int[] { R.id.name,
+                    R.id.email, R.id.mobile });
+
+            setListAdapter(adapter);
+
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/venkata/mampreciever/PhotoPage.java
@@ -0,0 +1,7 @@
+package com.example.venkata.mampreciever;
+
+/**
+ * Created by Venkata on 15-05-15.
+ */
+public class PhotoPage {
+}
